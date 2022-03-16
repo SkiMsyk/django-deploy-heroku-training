eaa15a746d916884ce67ef0e15f0ef7200a1f8a6
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-03-16 20:33:31 +0900

    fix settings for s3

--- myblog/settings.py
+++ myblog/settings.py
@@ -115,11 +115,18 @@ LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
 # S3 setting
+AWS_STORAGE_BUCKET_NAME = 'udemy-django-heroku-course'
+AWS_S3_CUSTOM_DOMAIN = '%s.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 # for deply at heroku
 django_heroku.settings(locals())
